39b4977768ba58ac82efe39b19c51cb7888a5a8e
Blake Warner <warnerianblake@gmail.com>
2020-10-11 19:01:25 -0400

    Update index.js
    functionality

--- index.js
+++ index.js
@@ -1,42 +1,34 @@
-navigator.getBattery().then(function(battery) {
-    function updateAllBatteryInfo(){
-      updateChargeInfo();
-      updateLevelInfo();
-      updateChargingInfo();
-      updateDischargingInfo();
-    }
-    updateAllBatteryInfo();
+ navigator.getBattery().then(function(battery) {
+     function updateAllBatteryInfo(){
+       updateChargeInfo();
+       updateLevelInfo();
+     }
+     updateAllBatteryInfo();
   
-    battery.addEventListener('chargingchange', function(){
-      updateChargeInfo();
-    });
-    function updateChargeInfo(){
-      console.log("Battery charging? "
-                  + (battery.charging ? "Yes" : "No"));
-    }
+     battery.addEventListener('chargingchange', function(){
+       updateChargeInfo();
+     });
+     function updateChargeInfo(){
+       console.log("Battery charging? "
+                   + (battery.charging ? "Yes" : "No"));
+       if(battery.charging == true){
+            document.getElementById("icon").style.display = true;
+       }
+     }
   
-    battery.addEventListener('levelchange', function(){
-      updateLevelInfo();
-    });
-    function updateLevelInfo(){
-      console.log("Battery level: "
-                  + battery.level * 100 + "%");
-    }
-  
-    battery.addEventListener('chargingtimechange', function(){
-      updateChargingInfo();
-    });
-    function updateChargingInfo(){
-      console.log("Battery charging time: "
-                   + battery.chargingTime + " seconds");
-    }
-  
-    battery.addEventListener('dischargingtimechange', function(){
-      updateDischargingInfo();
-    });
-    function updateDischargingInfo(){
-      console.log("Battery discharging time: "
-                   + battery.dischargingTime + " seconds");
-    }
-  
-  });
+     battery.addEventListener('levelchange', function(){
+       updateLevelInfo();
+     });
+     function updateLevelInfo(){
+       console.log("Battery level: "
+                   + battery.level * 100 + "%");
+                   var status = battery.level * 100 +"%";
+                   document.getElementById("batteryLevel").style.width = status;
+                   document.getElementById("percent").innerText = status;
+
+
+     }  
+   });
+
+ 
+
